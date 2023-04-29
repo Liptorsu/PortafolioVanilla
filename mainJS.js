84433e462131c24553acfb46896698b96ce89b2d
Liptorsu <sebatplip@gmail.com>
2023-04-29 18:31:39 -0300

    5to

--- mainJS.js
+++ mainJS.js
@@ -3,9 +3,15 @@ const d = document;
 const $menuResponsive = d.querySelector(".menu-responsive"),
         $btnHamburguer = d.querySelector(".hamburguer-menu")
 
+const $project = d.querySelector(".project-tittle");
+
+
 d.addEventListener("click", (e)=>{
    if (e.target===$btnHamburguer){
     $menuResponsive.classList.toggle("menu-plegable");
     console.log(e.target)
+   }
+        if (e.target === $project) {
+                window.scroll(0,900)
    }
 })
